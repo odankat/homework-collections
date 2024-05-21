c565e7adaf9a4394623e1bd5b0ceb91fe94dad73
HomeMedia <taknado13@yandex.ru>
2024-05-21 23:21:34 +0700

    version 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        System.out.println(nums);
 
     }
 }
